ae9ad362efcc526189e867c0214290a899bbb47e
Automated workflow <noreply@piranha.cloud>
2023-11-12 09:11:28 -0600

    Fixes #183 - Split UI into separate package

--- src/main/java/com/manorrock/aegean/ui/BytesConverter.java
+++ src/main/java/com/manorrock/aegean/ui/BytesConverter.java
@@ -25,7 +25,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-package com.manorrock.aegean;
+package com.manorrock.aegean.ui;
 
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
--- src/main/java/com/manorrock/aegean/ui/IndexPage.java
+++ src/main/java/com/manorrock/aegean/ui/IndexPage.java
@@ -23,8 +23,10 @@
  *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  *  POSSIBILITY OF SUCH DAMAGE.
  */
-package com.manorrock.aegean;
+package com.manorrock.aegean.ui;
 
+import com.manorrock.aegean.Repository;
+import com.manorrock.aegean.Application;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
