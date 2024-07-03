3b883861dc0a86980ee35b9ec5861c7a33dcf62a
Manfred Riem <mriem@manorrock.com>
2024-07-03 16:22:43 -0500

    Fixes #201 - Separate web UI and Git HTTP functionality in separate web applications (#202)

---
+++ ui/webapp/api/api/README.md
@@ -0,0 +1,51 @@
+# Manorrock Aegean
+
+[![build](https://github.com/manorrock/aegean/actions/workflows/build.yml/badge.svg)](https://github.com/manorrock/aegean/actions/workflows/build.yml)
+
+This project delivers a HTTP-based Git repository server.
+
+## Deploy the WAR file
+
+Deploy the WAR file on your Servlet container / application server as per the
+instructions of your vendor.
+
+## Running using the container image from DockerHub
+
+In an empty directory of your choice use the following command line to start 
+Manorrock Aegean.
+
+```shell
+  docker run --rm -d -it -p 8080:8080 -v $PWD:/mnt ghcr.io/manorrock/aegean
+```
+
+## Verify the server is up and running
+
+To verify the container is up and running execute the command below:
+
+```
+git clone http://localhost:8080/repositories/test.git
+```
+
+You should see output similar to below:
+
+```
+Cloning into 'test'...
+warning: You appear to have cloned an empty repository.
+```
+
+Congratulations you are now running Manorrock Aegean!
+
+## How do I contribute?
+
+See [Contributing](CONTRIBUTING.md)
+
+## Our code of Conduct
+
+See [Code of Conduct](CODE_OF_CONDUCT.md)
+
+## Important notice
+
+Note if you file issues or answer questions on the issue tracker and/or issue 
+pull requests you agree that those contributions will be owned by Manorrock.com
+and that Manorrock.com can use those contributions in any manner Manorrock.com
+so desires.
---
+++ ui/webapp/api/src/main/java/com/manorrock/aegean/ui/RepositoryViewPage.java
@@ -0,0 +1,150 @@
+/*
+ *  Copyright (c) 2002-2024, Manorrock.com. All Rights Reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *
+ *      1. Redistributions of source code must retain the above copyright
+ *         notice, this list of conditions and the following disclaimer.
+ *
+ *      2. Redistributions in binary form must reproduce the above copyright
+ *         notice, this list of conditions and the following disclaimer in the
+ *         documentation and/or other materials provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ *  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ *  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ *  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.manorrock.aegean.ui;
+
+import com.manorrock.aegean.Repository;
+import com.manorrock.aegean.Application;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import javax.annotation.PostConstruct;
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+import javax.faces.annotation.RequestParameterMap;
+import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
+
+/**
+ * The CDI bean for the /repository/view.xhtml page.
+ *
+ * @author Manfred Riem (mriem@manorrock.com)
+ */
+@Named(value = "repositoryViewPage")
+@Dependent
+public class RepositoryViewPage {
+
+    /**
+     * Stores the application.
+     */
+    @Inject
+    private Application application;
+
+    /**
+     * Stores the clone URL.
+     */
+    private String cloneUrl;
+
+    /**
+     * Stores the commits.
+     */
+    private List<RevCommit> commits;
+
+    /**
+     * Stores the name.
+     */
+    @RequestParameterMap
+    @Inject
+    private Map parameterMap;
+
+    /**
+     * Stores the repository.
+     */
+    private Repository repository;
+
+    /**
+     * Stores the request.
+     */
+    @Inject
+    private HttpServletRequest request;
+
+    /**
+     * Get the clone URL.
+     *
+     * @return the clone URL.
+     */
+    public String getCloneUrl() {
+        return cloneUrl;
+    }
+
+    /**
+     * Get the commits.
+     *
+     * @return the commits.
+     */
+    public List<RevCommit> getCommits() {
+        return commits;
+    }
+
+    /**
+     * Get the repository.
+     *
+     * @return the repository.
+     */
+    public Repository getRepository() {
+        return repository;
+    }
+
+    /**
+     * Initialize.
+     */
+    @PostConstruct
+    public void initialize() {
+        repository = application.getRepository((String) parameterMap.get("name"));
+        commits = new ArrayList<>();
+        try {
+            org.eclipse.jgit.lib.Repository gitRepository = new FileRepositoryBuilder()
+                    .setGitDir(new File(application.getRepositoriesDirectory(), repository.getName()))
+                    .findGitDir()
+                    .build();
+            try ( RevWalk revWalk = new RevWalk(gitRepository)) {
+                ObjectId gitObjectId = gitRepository.resolve(gitRepository.getBranch());
+                if (gitObjectId == null) {
+                    gitObjectId = gitRepository.resolve(gitRepository.getFullBranch());
+                }
+                if (gitObjectId != null) {
+                    revWalk.markStart(revWalk.parseCommit(gitObjectId));
+                    for (RevCommit commit : revWalk) {
+                        commits.add(commit);
+                    }
+                }
+            }
+        } catch (IOException ioe) {
+
+        }
+        cloneUrl = 
+                request.getScheme() + "://" +
+                request.getServerName() +
+                (request.getServerPort() == 80 ? "" : 
+                    (request.getServerPort() == 443 ? "" : ":" + request.getServerPort())) + 
+                "/repositories/" + repository.getName();
+    }
+}
